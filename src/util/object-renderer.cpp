3301eaa5c5a78e9b1f1911ba7f1e07f34eacff01
riteshco <riteshgadwalv2@gmail.com>
2026-01-31 00:14:46 +0530

    Fix #6040: svg with markers which dont have associated path data now don't cause issues
    Inkscape crashed when rendering the marker preview if a marker element
    contained no children(nullptr). This adds a precondition check to ensure
    the child exists before attempting to read style attributes.
    
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/6040

--- src/util/object-renderer.cpp
+++ src/util/object-renderer.cpp
@@ -341,6 +341,10 @@ Cairo::RefPtr<Cairo::Surface> create_marker_image(
         return g_bad_marker;
     }
 
+    if (!marker->firstChild()) {
+        return g_bad_marker;
+    }
+
     SPObject *oldmarker = _sandbox->getObjectById("sample");
     if (oldmarker) {
         oldmarker->deleteObject(false);
