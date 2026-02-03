d0934fec445636c038473aa1c5de8efe02f72faf
समीर सिंह Sameer Singh <lumarzeli30@gmail.com>
2026-02-03 05:40:52 +0530

    Fix crash in connector endpoint handling with null curve

--- src/object/sp-conn-end-pair.cpp
+++ src/object/sp-conn-end-pair.cpp
@@ -227,7 +227,7 @@ void SPConnEndPair::getEndpoints(Geom::Point endPts[]) const
     for (unsigned h = 0; h < 2; ++h) {
         if (h2attItem[h]) {
             endPts[h] = h2attItem[h]->getAvoidRef().getConnectionPointPos();
-        } else if (!curve->is_empty()) {
+        } else if (curve && !curve->is_empty()) {
             if (h == 0) {
                 endPts[h] = *(curve->first_point()) * i2d;
             } else {
