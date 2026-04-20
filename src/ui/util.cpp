9df39b1a7c95160b3275a7ded55f2f5f3fa01893
Wen-Wei Kao <ltlnx@disroot.org>
2026-04-20 17:38:32 +0800

    Test Windows IME enable/disable, pass 4

--- src/ui/util.cpp
+++ src/ui/util.cpp
@@ -479,11 +479,15 @@ void set_dark_titlebar(Glib::RefPtr<Gdk::Surface> const &surface, bool is_dark)
 void set_windows_ime_enabled(Glib::RefPtr<Gdk::Surface> const &surface, bool enabled)
 {
 #if (defined (_WIN32) || defined (_WIN64))
+    std::cerr << "set_windows_ime_enabled" << std::endl;
     if (surface->gobj()) {
+        std::cerr << "set_windows_ime_enabled if" << std::endl;
         HWND hwnd = (HWND)gdk_win32_surface_get_handle((GdkSurface*)surface->gobj());
         if (enabled) {
+            std::cerr << "set_windows_ime_enabled enabled" << std::endl;
             ImmAssociateContextEx(hwnd, nullptr, IACE_DEFAULT);
         } else {
+            std::cerr << "set_windows_ime_enabled disabled" << std::endl;
             ImmAssociateContext(hwnd, nullptr);
         }
     }
