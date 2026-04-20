b7362a98c9436fe0240e03ce513e4a36e6b4ba4c
Wen-Wei Kao <ltlnx@disroot.org>
2026-04-20 17:52:48 +0800

    Test Windows IME enable/disable, pass 5

--- src/ui/util.cpp
+++ src/ui/util.cpp
@@ -480,7 +480,7 @@ void set_windows_ime_enabled(Glib::RefPtr<Gdk::Surface> const &surface, bool ena
 {
 #if (defined (_WIN32) || defined (_WIN64))
     std::cerr << "set_windows_ime_enabled" << std::endl;
-    if (surface->gobj()) {
+    // if (surface->gobj()) {
         std::cerr << "set_windows_ime_enabled if" << std::endl;
         HWND hwnd = (HWND)gdk_win32_surface_get_handle((GdkSurface*)surface->gobj());
         if (enabled) {
@@ -490,7 +490,7 @@ void set_windows_ime_enabled(Glib::RefPtr<Gdk::Surface> const &surface, bool ena
             std::cerr << "set_windows_ime_enabled disabled" << std::endl;
             ImmAssociateContext(hwnd, nullptr);
         }
-    }
+    // }
 #endif
 }
 
