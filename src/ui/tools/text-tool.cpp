6868239fdca1969e61016aad05d0ee7c589bbb1b
Wen-Wei Kao <ltlnx@disroot.org>
2026-04-30 00:29:56 +0800

    Revert "Remove cursor show code from _updateCursor"
    This reverts commit 46bc360b68c6866925e2811a81aa97094e1a20b9.

--- src/ui/tools/text-tool.cpp
+++ src/ui/tools/text-tool.cpp
@@ -1540,6 +1540,7 @@ void TextTool::_updateCursor(bool scroll_to_see)
         }
 
         cursor->set_coords(d0, d1);
+        _showCursor();
 
         /* fixme: ... need another transformation to get canvas widget coordinate space? */
         if (imc) {
