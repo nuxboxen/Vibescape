nuxboxen/Vibescape

commit 1ace6c81758225f685fe5f4c28f6914597767ce7

Stop straight bspline corners from becoming curves when moved
When creating the handles for a curve, only one of the two handles of a
sharp edge are coming in as zero-length - so check for that as we
create the handles and normalize both handles to zero if needed.

Fixes https://gitlab.com/inkscape/inkscape/-/work_items/5794

src/ui/tool/path-manipulator.cpp
@@ -1237,6 +1237,18 @@ Geom::Affine PathManipulator::_getTransform() const
     return _i2d_transform * _edit_transform;
 }
 
+void PathManipulator::_normalizeBsplineHandles(Node *n)
+{
+    // If either handle is degenerate, make them both degenerate. When loading a bspline curve,
+    // only one side might start degenerate (which isn't a possible situation in the UI - they
+    // should both be or neither be) and we need to do this to fix it.
+    if (n->front()->isDegenerate()) {
+        n->back()->setPosition(n->position());
+    } else if (n->back()->isDegenerate()) {
+        n->front()->setPosition(n->position());
+    }
+}
+
 /** Create nodes and handles based on the XML of the edited path. */
 void PathManipulator::_createControlPointsFromGeometry()
 {
@@ -1301,6 +1313,21 @@ void PathManipulator::_createControlPointsFromGeometry()
             {
                 previous_node->front()->setPosition((*bezier)[1]);
                 current_node ->back() ->setPosition((*bezier)[2]);
+
+                // Once we have set the position of both handles of a node, normalize that node.
+                if (previous_node != subpath->begin().get_pointer()) {
+                    // Not the first segment, so both handles of previous node are now set.
+                    // This will hit on every segment but the first one, leaving the first node
+                    // to be handled in the check below (or never if we are an open path).
+                    _normalizeBsplineHandles(previous_node);
+                }
+                if (current_node == subpath->begin().get_pointer()) {
+                    // Last segment, so the very first node now has both handles set.
+                    // This will only hit for closed paths, where this will finally close the first
+                    // node after we ignored it above. Open paths won't have their two edge nodes
+                    // finalized because the last segment closing the path isn't in this loop.
+                    _normalizeBsplineHandles(current_node);
+                }
             }
             previous_node = current_node;
         }

src/ui/tool/path-manipulator.h
@@ -135,6 +135,7 @@ private:
     Geom::Point _bsplineHandleReposition(Handle *h, bool check_other = true);
     Geom::Point _bsplineHandleReposition(Handle *h, double pos);
     void _createGeometryFromControlPoints(bool alert_LPE = false);
+    void _normalizeBsplineHandles(Node *n);
     unsigned _deleteStretch(NodeList::iterator first, NodeList::iterator last, NodeDeleteMode mode);
     std::string _createTypeString();
     void _updateOutline();

commit 5a39151e0762ddb078e042431493571b1fe792f1

Limit bspline fix to bsplines.
Fixes bug introduced by 1ace6c81758225f685fe5f4c28f6914597767ce7

Fix for #6352

src/ui/tool/path-manipulator.cpp
@@ -1315,19 +1315,21 @@ void PathManipulator::_createControlPointsFromGeometry()
                 previous_node->front()->setPosition((*bezier)[1]);
                 current_node ->back() ->setPosition((*bezier)[2]);
 
-                // Once we have set the position of both handles of a node, normalize that node.
-                if (previous_node != subpath->begin().get_pointer()) {
-                    // Not the first segment, so both handles of previous node are now set.
-                    // This will hit on every segment but the first one, leaving the first node
-                    // to be handled in the check below (or never if we are an open path).
-                    _normalizeBsplineHandles(previous_node);
-                }
-                if (current_node == subpath->begin().get_pointer()) {
-                    // Last segment, so the very first node now has both handles set.
-                    // This will only hit for closed paths, where this will finally close the first
-                    // node after we ignored it above. Open paths won't have their two edge nodes
-                    // finalized because the last segment closing the path isn't in this loop.
-                    _normalizeBsplineHandles(current_node);
+                if (_is_bspline) {
+                    // Once we have set the position of both handles of a node, normalize that node.
+                    if (previous_node != subpath->begin().get_pointer()) {
+                        // Not the first segment, so both handles of previous node are now set.
+                        // This will hit on every segment but the first one, leaving the first node
+                        // to be handled in the check below (or never if we are an open path).
+                        _normalizeBsplineHandles(previous_node);
+                    }
+                    if (current_node == subpath->begin().get_pointer()) {
+                        // Last segment, so the very first node now has both handles set.
+                        // This will only hit for closed paths, where this will finally close the first
+                        // node after we ignored it above. Open paths won't have their two edge nodes
+                        // finalized because the last segment closing the path isn't in this loop.
+                        _normalizeBsplineHandles(current_node);
+                    }
                 }
             }
             previous_node = current_node;