96f61d5c2e15c3c28ae97595181d935a3badf850
Michael Terry <mike@mterry.name>
2026-09-22 12:03:39 -0400

    Fix flowroot/flowtext support in capy pdf exporter
    Fixes https://gitlab.com/inkscape/inkscape/-/work_items/6132

--- src/extension/internal/pdfoutput/build-drawing.cpp
+++ src/extension/internal/pdfoutput/build-drawing.cpp
@@ -160,7 +160,7 @@ void DrawContext::paint_item(SPItem const *item, Geom::Affine const &tr, SPStyle
     } else if (auto text = cast<SPText>(item)) {
         paint_text_layout(text->layout, context_style);
     } else if (auto flowtext = cast<SPFlowtext>(item)) {
-        // sp_flowtext_render(flowtext);
+        paint_text_layout(flowtext->layout, context_style);
     } else if (auto image = cast<SPImage>(item)) {
         paint_raster(image);
     } else if (auto group = cast<SPGroup>(item)) { // SPSymbol, SPRoot, SPMarker
